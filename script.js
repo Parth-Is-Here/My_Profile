21f44206d92fdff271d0de32fdae38aa00fe8214
Parth Kumar Sahu <radhe.radhe.parth.here.18@gmail.com>
2023-06-19 14:25:46 +0530

    git commit
    another one

---
+++ script.js
@@ -0,0 +1,21 @@
+function changeMode(){
+    let wholeBody = document.body;
+    wholeBody.classList.toggle('body-light');
+    wholeBody.classList.toggle('text-dark');
+
+    let mapLight = document.querySelector('.location');
+    mapLight.classList.toggle('map-light');
+
+    let profilePic = document.querySelector('.profilepic');
+    profilePic.classList.toggle('profilee');
+
+    let opaPic = document.querySelector('.profile-img');
+    opaPic.classList.toggle('opaState');
+
+    let lang_logo_bg = document.querySelector('.part-of-sec-3');
+    lang_logo_bg.classList.toggle('theme-part-sec-3');
+}
+
+function changeColor(){
+    
+}
